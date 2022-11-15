a079548c1d2710a7ab3ed850975af725fede3a2b
jael137 <jaelkwakkel@gmail.com>
2022-11-15 13:17:01 +0100

    Geluid afspelen

--- VirtualPiano/Program.cs
+++ VirtualPiano/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Media;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Test(1);
+        SoundPlayer snd = new SoundPlayer("../../../Sounds/639466__timouse__piano-loop-7.wav");
+        snd.Play();
+
+        for (; ; )
+        {
+            Thread.Sleep(100);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns>The number zero</returns>
+    public static int Test(int x)
+    {
+        if (true) Console.WriteLine("test");
+        Console.WriteLine(x);
+        return 0;
+    }
+}
