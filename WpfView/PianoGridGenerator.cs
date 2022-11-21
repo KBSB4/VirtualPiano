012fdbd98119199104f03b68fe738aaf9cfb479f
KeesW123 <91602285+KeesW123@users.noreply.github.com>
2022-11-21 15:05:12 +0100

    PH-6 deleted double create color method

--- WpfView/PianoGridGenerator.cs
+++ WpfView/PianoGridGenerator.cs
@@ -88,16 +88,5 @@ namespace WpfView
 
             return new SolidColorBrush(Color.FromRgb(r, g, b));
         }
-
-        private static SolidColorBrush CreateColor()
-        {
-            var random = new Random();
-
-            var r = Convert.ToByte(random.Next(0, 255));
-            var g = Convert.ToByte(random.Next(0, 255));
-            var b = Convert.ToByte(random.Next(0, 255));
-
-            return new SolidColorBrush(Color.FromRgb(r, g, b));
-        }
     }
 }
