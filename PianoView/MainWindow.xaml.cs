36c1a3c5c6af66529a1f8cdba51d1759d09a9674
Femke Spanner <s1147381@student.windesheim.nl>
2022-11-21 23:18:13 +0100

    Rewriten code US1.1 toetsenbord

---
+++ PianoView/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VirtualPiano;
+
+namespace PianoView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Piano piano = new Piano();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.KeyDown += KeyPressed;
+            this.KeyUp += KeyReleased;
+        }
+
+
+
+        public void KeyPressed(object source, KeyEventArgs e)
+        {
+            int intValue = (int)e.Key;
+            if (Keyboard.IsKeyDown(Key.RightShift) || Keyboard.IsKeyDown(Key.LeftShift))
+            {
+                foreach (var key in piano.CustomKeys)
+                {
+                    if (key.MicrosoftBind == intValue && key.KeyBindChar.ToString().Equals(e.Key.ToString().ToUpper()))
+                    {
+                        key.PressedDown = true;
+                        b.Text = key.KeyBindChar.ToString();
+                    }
+                }
+            }
+            else
+            {
+
+                foreach (var key in piano.CustomKeys)
+                {
+                    if (key.MicrosoftBind == intValue && key.KeyBindChar.ToString().Equals(e.Key.ToString().ToLower()))
+                    {
+                        key.PressedDown = true;
+                        b.Text = key.KeyBindChar.ToString();
+                    }
+                }
+
+            }
+
+        }
+
+
+        public void KeyReleased(object source, KeyEventArgs e)
+        {
+            int intValue = (int)e.Key;
+            if (Keyboard.IsKeyDown(Key.RightShift) || Keyboard.IsKeyDown(Key.LeftShift))
+            {
+                foreach (var key in piano.CustomKeys)
+                {
+                    if (key.MicrosoftBind == intValue && key.KeyBindChar.ToString().Equals(e.Key.ToString().ToUpper()))
+                    {
+                        key.PressedDown = false;
+                    }
+                }
+            }
+            else
+            {
+
+                foreach (var key in piano.CustomKeys)
+                {
+                    if (key.MicrosoftBind == intValue && key.KeyBindChar.ToString().Equals(e.Key.ToString().ToLower()))
+                    {
+                        key.PressedDown = false;
+                    }
+                }
+
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ VirtualPiano/Piano.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPiano
+{
+    public class Piano
+    {
+        private int[] MicrosoftKeybinds = { 60, 66, 48, 61, 63, 68, 64, 52, 58, 59, 44, 62, 47, 49, 50, 51, 53, 54, 55, 69, 67, 46, 65, 45, 57, 56 };
+
+        static bool up;
+
+        public List<CustomKey> CustomKeys { get; set; }
+
+
+        public Piano()
+        {
+            CustomKeys = new();
+            AssembleKeyBindings();
+
+        }
+
+
+
+
+
+
+        public void UpdateOctaveAndNote(ref Notes currentNote, ref Octaves currentOctave)
+        {
+            if (currentNote.Equals(Notes.Unknown))
+            {
+                currentNote = Notes.A;
+                currentOctave++;
+            }
+        }
+
+        public void CreateKey(Octaves octave, ref Notes note, int getal)
+        {
+            if (!up)
+            {
+                CustomKeys.Add(new CustomKey(octave, note, getal) { KeyBindChar = (char)((char)getal + 53) });
+            }
+            else
+            {
+                CustomKeys.Add(new CustomKey(octave, note, getal) { KeyBindChar = (char)((char)getal + 21) });
+
+            }
+            note++;
+        }
+
+
+        public void AssembleKeyBindings()
+        {
+            Octaves currentoctave = 0;  // first octave two
+            Notes currentnote = Notes.C; // first key of the virtual keyboard
+
+            for (int j = 0; j < 24; j++)
+            {
+
+                CreateKey(currentoctave, ref currentnote, MicrosoftKeybinds[j]);
+                UpdateOctaveAndNote(ref currentnote, ref currentoctave);
+                up = true;
+                CreateKey(currentoctave, ref currentnote, MicrosoftKeybinds[j]);
+                UpdateOctaveAndNote(ref currentnote, ref currentoctave);
+                up = false;
+
+            }
+
+
+        }
+
+
+    }
+}
