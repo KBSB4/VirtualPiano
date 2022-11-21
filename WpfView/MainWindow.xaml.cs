bad63d60cfc6827a4692fd02bc17a234c3ecb14d
jael137 <jaelkwakkel@gmail.com>
2022-11-21 12:39:59 +0100

    Start of rows

---
+++ WpfView/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace WpfView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            new PianoGridGenerator(TestGrid, 2, 5);
+        }
+    }
+}
---
+++ WpfView/PianoGridGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfView
+{
+    internal class PianoGridGenerator
+    {
+
+        public PianoGridGenerator(Grid grid, int rowAmount, int columnAmount)
+        {
+            if (rowAmount < 0 || columnAmount < 0)
+            {
+                return;
+            }
+
+            grid.RowDefinitions.Clear();
+            grid.ColumnDefinitions.Clear(); ;
+
+            for (int i = 0; i < rowAmount; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
+            }
+            for (int i = 0; i < columnAmount; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
+            }
+
+            Button element = new Button() { Background = Brushes.Aqua, Width =  };
+            Grid.SetColumn(element, 4);
+            grid.Children.Add(element);
+        }
+    }
+}
