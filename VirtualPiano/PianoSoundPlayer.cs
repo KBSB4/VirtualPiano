165540a887d9dd8d175c69afdd2c749fa9f97fcf
sandordost <sandor1998@hotmail.com>
2022-11-21 15:13:20 +0100

    PH-1: Initial Push of Working PianoPlayer
    The PianoSoundPlayer was created (initial form). This class creates and manages piano sounds

---
+++ VirtualPiano/PianoSoundPlayer.cs
@@ -0,0 +1,116 @@
+﻿using Melanchall.DryWetMidi.MusicTheory;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using SharpDX.Multimedia;
+using SharpDX.XAudio2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPiano
+{
+	public class PianoSoundPlayer
+	{
+		private string pianoFilesFolder;
+		private string pianoSoundPrefix;
+		private string pianoSoundSuffix;
+
+
+		/// <summary>
+		/// The <paramref name="soundsFolder"/> is the folder that contains all the piano files.
+		/// File example: "myPiano-CSharp.wav"
+		/// The <paramref name="pianoSoundSuffix"/> is first part of the file ("myPiano-" in example.)
+		/// The <paramref name="pianoSoundSuffix"/> is the last part / extension of the file. (".wav" in example)
+		/// Make sure the note name part is spelled according to the DryWetMidi NoteName enum
+		/// </summary>
+		/// <param name="soundsFolder"></param>
+		/// <param name="pianoSoundPrefix"></param>
+		/// <param name="pianoSoundSuffix"></param>
+		public PianoSoundPlayer(string soundsFolder, string pianoSoundPrefix, string pianoSoundSuffix)
+		{
+			pianoFilesFolder = soundsFolder;
+			this.pianoSoundPrefix = pianoSoundPrefix;
+			this.pianoSoundSuffix = pianoSoundSuffix;
+		}
+
+		public void PlayNote(NoteName noteName, int octave)
+		{
+			float frequency = (float)((float)1 / 1024 * (((float)octave + 5) * 100));
+			string pianoNoteString = noteName.ToString();
+			string pathToFile = pianoFilesFolder + pianoSoundPrefix + pianoNoteString + pianoSoundSuffix;
+			PlaySoundOneshot(pathToFile, frequency);
+		}
+
+		public void PlaySoundOneshot(string audioFile, float frequency)
+		{
+			GetAudioClip(audioFile, frequency).Start();
+		}
+
+		public SourceVoice GetAudioClip(string audioFile, float frequency)
+		{
+			XAudio2 device = new XAudio2();
+			MasteringVoice masteringVoice = new MasteringVoice(device);
+			var stream = new SoundStream(File.OpenRead(audioFile));
+			var waveFormat = stream.Format;
+			var buffer = new AudioBuffer
+			{
+				Stream = stream.ToDataStream(),
+				AudioBytes = (int)stream.Length,
+				Flags = BufferFlags.EndOfStream
+			};
+
+			var sourceVoice = new SourceVoice(device, waveFormat, true);
+			sourceVoice.SetFrequencyRatio(frequency);
+			sourceVoice.BufferEnd += (context) => Console.WriteLine(" => event received: end of buffer");
+			sourceVoice.SubmitSourceBuffer(buffer, stream.DecodedPacketsInfo);
+
+			return sourceVoice;
+		}
+
+		public FadingAudio GetFadingAudio(NoteName noteName, int octave)
+		{
+			float frequency = (float)((float)1 / 1024 * (((float)octave + 5) * 100));
+			string pianoNoteString = noteName.ToString();
+			string pathToFile = pianoFilesFolder + pianoSoundPrefix + pianoNoteString + pianoSoundSuffix;
+			return new FadingAudio(GetAudioClip(pathToFile, frequency));
+		}
+
+		public class FadingAudio {
+			private SourceVoice sourceVoice;
+
+			public FadingAudio()
+			{
+
+			}
+
+			public FadingAudio(SourceVoice sourceVoice)
+			{
+				this.sourceVoice = sourceVoice;
+			}
+
+			public void StartPlaying()
+			{
+				sourceVoice.Start();
+			}
+
+			public void StopPlaying(float fadeOutSpeed)
+			{
+				new Thread(() =>
+				{
+					float volume = 0;
+					sourceVoice.GetVolume(out volume);
+					while (volume > 0) {
+						volume -= fadeOutSpeed / 1000;
+						sourceVoice.SetVolume(volume);
+						Thread.Sleep(10);
+					}
+					sourceVoice.Stop();
+					sourceVoice.Dispose();
+				}).Start();
+			}
+		}
+	}
+}
--- VirtualPiano/Program.cs
+++ VirtualPiano/Program.cs
@@ -1,29 +1,90 @@
-﻿using System.Media;
+﻿using Melanchall.DryWetMidi.MusicTheory;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using SharpDX.Multimedia;
+using SharpDX.XAudio2;
+using SharpDX.XAudio2.Fx;
+using System;
+using System.Media;
+using System.Windows.Input;
+using VirtualPiano;
 
 internal class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        Test(1);
-        SoundPlayer snd = new SoundPlayer("../../../Sounds/639466__timouse__piano-loop-7.wav");
-        snd.Play();
 
-        for (; ; )
-        {
-            Thread.Sleep(100);
-        }
-    }
+	private const string _pianoFilesFolder = "../../../Sounds/Piano/";
+	private const string _pianoFileSuffix = "";
+	private const string _pianoFilePrefix = ".wav";
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="x"></param>
-    /// <returns>The number zero</returns>
-    public static int Test(int x)
-    {
-        if (true) Console.WriteLine("test");
-        Console.WriteLine(x);
-        return 0;
-    }
+	private static void Main(string[] args)
+	{
+		var player = new PianoSoundPlayer(_pianoFilesFolder, _pianoFilePrefix, _pianoFileSuffix);
+
+		for (; ; )
+		{
+			if (Console.KeyAvailable)
+			{
+				ConsoleKey key = Console.ReadKey(true).Key;
+				switch (key)
+				{
+					case ConsoleKey.Q:
+						player.PlayNote(NoteName.C, 4);
+						break;
+					case ConsoleKey.D2:
+						player.PlayNote(NoteName.CSharp, 4);
+						break;
+					case ConsoleKey.W:
+						player.PlayNote(NoteName.D, 4);
+						break;
+					case ConsoleKey.D3:
+						player.PlayNote(NoteName.DSharp, 4);
+						break;
+					case ConsoleKey.E:
+						player.PlayNote(NoteName.E, 4);
+						break;
+					case ConsoleKey.R:
+						player.PlayNote(NoteName.F, 4);
+						break;
+					case ConsoleKey.D5:
+						player.PlayNote(NoteName.FSharp, 4);
+						break;
+					case ConsoleKey.T:
+						player.PlayNote(NoteName.G, 4);
+						break;
+					case ConsoleKey.D6:
+						player.PlayNote(NoteName.GSharp, 4);
+						break;
+					case ConsoleKey.Y:
+						player.PlayNote(NoteName.A, 4);
+						break;
+					case ConsoleKey.D7:
+						player.PlayNote(NoteName.ASharp, 4);
+						break;
+					case ConsoleKey.U:
+						player.PlayNote(NoteName.B, 4);
+						break;
+				}
+			}
+			Thread.Sleep(10);
+		}
+	}
+
+	
+
+	public static bool IsKeyPressed(ConsoleKey key)
+	{
+		return Console.KeyAvailable && Console.ReadKey(true).Key == key;
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="x"></param>
+	/// <returns>The number zero</returns>
+	public static int Test(int x)
+	{
+		if (true) Console.WriteLine("test");
+		Console.WriteLine(x);
+		return 0;
+	}
 }
