42d1db9842fd3cefddd93e67416a337b53f8b748
KeesW123 <s1173714@student.windesheim.nl>
2022-11-24 13:17:41 +0100

    added test classes and renamed one

---
+++ UnitTests_PianoSoundPlayer/Controller_PianoController_Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class Controller_PianoController_Tests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
