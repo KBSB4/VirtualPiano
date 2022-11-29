8f6046f1a03eb9860118629caf7f654c82b789a2
jael137 <jaelkwakkel@gmail.com>
2022-11-29 21:23:17 +0100

    Changed to pages main menu finished an navigationable

---
+++ WpfView/FreePlayPiano.xaml.cs
@@ -0,0 +1,64 @@
+﻿using BusinessLogic;
+using Controller;
+using Model;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfView
+{
+    /// <summary>
+    /// Interaction logic for FreePlayPiano.xaml
+    /// </summary>
+    public partial class FreePlayPiano : Page
+    {
+        PianoGridGenerator pianoGrid;
+
+        public FreePlayPiano()
+        {
+            InitializeComponent();
+            pianoGrid = new PianoGridGenerator(WhiteKeysGrid, BlackKeysGrid, 28);
+            PianoController.CreatePiano();
+
+            //Add keydown event for the keys
+            this.KeyDown += KeyPressed;
+            this.KeyUp += KeyReleased;
+        }
+
+        /// <summary>
+        /// Eventhandler for when the key gets pressed. Updates key and plays the audio
+        /// Updates the view
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
+        public void KeyPressed(object source, KeyEventArgs e)
+        {
+            int intValue = (int)e.Key;
+
+            PianoKey? key = PianoController.GetPressedPianoKey(intValue);
+            if (key is not null)
+            {
+                pianoGrid.DisplayPianoKey(key, true);
+            }
+
+            if (e.Key == Key.CapsLock)
+                PianoLogic.SwapOctave(PianoController.Piano);
+        }
+
+        /// <summary>
+        /// If pressed down keyboard key gets released, stop the audio playing for the pianokey and unpress it.
+        /// Updates the view
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
+        public void KeyReleased(object source, KeyEventArgs e)
+        {
+            int intValue = (int)e.Key;
+            PianoKey? key = PianoController.GetReleasedKey(intValue);
+            if (key is not null)
+            {
+                pianoGrid.DisplayPianoKey(key, false);
+            }
+
+        }
+    }
+}
