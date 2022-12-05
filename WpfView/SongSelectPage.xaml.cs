fcee716b7e5dcc2ad1f8b675ab4bb50660dd6fc8
jael137 <jaelkwakkel@gmail.com>
2022-12-05 17:08:56 +0100

    Select number screen using songcards with title and difficulty

---
+++ WpfView/SongSelectPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace WpfView
+{
+    /// <summary>
+    /// Interaction logic for SongSelectPage.xaml
+    /// </summary>
+    public partial class SongSelectPage : Page
+    {
+        public SongSelectPage()
+        {
+            InitializeComponent();
+
+            AddSongs();
+        }
+
+        private void AddSongs()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                SongCardControl songCardControl = new(i.ToString(), i % 2);
+
+                SongCards.Children.Add(songCardControl);
+            }
+        }
+    }
+}
