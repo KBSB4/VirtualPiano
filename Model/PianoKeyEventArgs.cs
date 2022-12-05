be9696f8326cedf1e13632c2cc0a545929cabdba
Harris <16213031+Labhatorian@users.noreply.github.com>
2022-12-05 10:24:40 +0100

    Revert "Practice notes branch"

--- Model/PianoKeyEventArgs.cs
+++ Model/PianoKeyEventArgs.cs
@@ -1,22 +1,18 @@
-﻿using Melanchall.DryWetMidi.Interaction;
-
-namespace Model
+﻿namespace Model
 {
     public class PianoKeyEventArgs : EventArgs
     {
         public PianoKey Key { get; set; }
-        public MidiTimeSpan Offset { get; set; }
-
-
-		public PianoKeyEventArgs(PianoKey pianoKey) : this(pianoKey, (MidiTimeSpan)0)
-		{
-			
-		}
-
-		public PianoKeyEventArgs(PianoKey key, MidiTimeSpan offset)
+        public TimeSpan Offset { get; set; }
+        public PianoKeyEventArgs(PianoKey key, TimeSpan offset)
         {
             Key = key;
             Offset = offset;
+        }
+
+        public PianoKeyEventArgs(PianoKey pianoKey)
+        {
+            Key = pianoKey;
         }
     }
 }
