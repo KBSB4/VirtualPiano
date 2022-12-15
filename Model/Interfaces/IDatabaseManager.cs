2a378d8e30749a67e4d9aa108a959631073bfd8d
sandordost <sandor1998@hotmail.com>
2022-12-15 21:44:41 +0100

    Added DatabaseController and SQLDatabaseManager
    Songs are fetchable, removable and addable

---
+++ Model/Interfaces/IDatabaseManager.cs
@@ -0,0 +1,23 @@
+﻿using Model.DatabaseModels;
+
+namespace Model.Interfaces
+{
+    public interface IDatabaseManager
+    {
+		#region Songs
+        public Task<Song?> GetSong(string songname);
+
+		public Task<Song[]> GetAllSongs();
+
+		public Task UploadSong(Song song);
+
+		public Task DeleteSong(string songname);
+		#endregion
+
+		#region Users
+		public Task<User> GetUser(string username);
+		#endregion
+
+
+	}
+}
