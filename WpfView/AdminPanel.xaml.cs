22c4b725f68da2aad235929c78870fa3f32f8ee7
FemkeSpanner <90692319+FemkeSpanner@users.noreply.github.com>
2022-12-18 09:25:28 +0100

    Added admin panel to databaseconnector.

---
+++ WpfView/AdminPanel.xaml.cs
@@ -0,0 +1,64 @@
+﻿using BusinessLogic;
+using Controller;
+using Melanchall.DryWetMidi.Core;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfView
+{
+    /// <summary>
+    /// Interaction logic for AdminPanel.xaml
+    /// </summary>
+    public partial class AdminPanel : Page
+    {
+        public AdminPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        ///  Sets the midi-file that has been selected for <see cref=" MidiLogic.CurrentMidi"/>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UploadMidiFile_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "MIDI Files (*.mid)|*.mid",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            bool? fileOpened = openFileDialog.ShowDialog();
+            if (fileOpened == true)
+            {
+                //Get the path of specified file
+                MidiLogic.CurrentMidi = MidiFile.Read(openFileDialog.FileName);
+            }
+        }
+
+
+
+    }
+}
