1231db204072c4ef46df61259436f14ebeb52a14
Xandra10101 <36187678+Xandra10101@users.noreply.github.com>
2022-12-19 12:58:26 +0100

    Updated with Ssh connect

---
+++ BusinessLogic/ProgramSSH.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renci.SshNet;
+
+namespace BusinessLogic
+{
+    class ProgramSSH
+    {
+       public static void ExecuteSshConnection()
+        {
+            AuthenticationMethod method = new PasswordAuthenticationMethod("student", "Arena-Enclose8");
+            ConnectionInfo connectionInfo = new ConnectionInfo("145.44.234.89", "student", method);
+            SshClient client = new SshClient(connectionInfo);
+            if (!client.IsConnected)
+            {
+                Debug.WriteLine("Client is not connected yet");
+                client.Connect();
+                client.KeepAliveInterval = TimeSpan.FromHours(2);
+            }
+
+            SshCommand readCommand = client.RunCommand("uname -mrs");
+            Debug.WriteLine(readCommand.Result);
+            SshCommand writeCommand = client.RunCommand("mkdir \"/home/student/Desktop/ssh_output\"");
+            Thread.Sleep(1000);
+            
+
+        }
+    }
+}
