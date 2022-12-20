a5cdde43515e2f3f900e14613e8770884e9b3104
FemkeSpanner <90692319+FemkeSpanner@users.noreply.github.com>
2022-12-20 12:18:36 +0100

    Added field validations for admin panel.

--- BusinessLogic/ValidationLogic.cs
+++ BusinessLogic/ValidationLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Melanchall.DryWetMidi.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,51 @@ namespace BusinessLogic
     public static class ValidationLogic
     {
 
+
+
+
+
+
+
+
+        #region AdminPanel Field validation Logic
+        public static string AdminPanelValidateTitle(string title)
+        {
+            string errorMessage = string.Empty;
+            if (title.Length == 0)
+            {
+                errorMessage = "Title is required!";
+            }
+            else if (title.Length > 30)
+            {
+                errorMessage = "Title must be between 1 and 30 characters.";
+            }
+
+            return errorMessage;
+        }
+
+        public static string AdminPanelValidateDescription(string description)
+        {
+            string errorMessage = string.Empty;
+            if (description.Length > 65) errorMessage = "Description must be between 0 and 65 characters.";
+            return errorMessage;
+        }
+
+        public static string AdminPanelValidateDifficulty(string difficulty)
+        {
+            string errorMessage = string.Empty;
+            if (!int.TryParse(difficulty, out int difficultyValue) || !(difficultyValue > -1 && difficultyValue < 4)) errorMessage = "Difficulty must be number between 0 {easy}, 1 {medium}, 2 {hard} or 3 {hero}.";
+            return errorMessage;
+        }
+
+
+        public static string AdminPanelValidateMidiFile(MidiFile file)
+        {
+            string errorMessage = string.Empty;
+            if (MidiLogic.CurrentMidi == null) errorMessage = "MidiFile required!";
+            return errorMessage;
+        }
+        #endregion
+
     }
-}
+    }
--- Controller/ValidationController.cs
+++ Controller/ValidationController.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using BusinessLogic;
+using Melanchall.DryWetMidi.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static BusinessLogic.ValidationLogic;
 
 namespace Controller
 {
     public static class ValidationController
     {
+
+
+
+
+        public static string AdminPanelValidationMessageTitle(string title)
+        {
+            return ValidationLogic.AdminPanelValidateTitle(title);
+        }
+
+        public static string AdminPanelValidationMessageDescription(string desc)
+        {
+            return ValidationLogic.AdminPanelValidateDescription(desc);
+        }
+
+        public static string AdminPanelValidationMessageDifficulty(string diff)
+        {
+            return ValidationLogic.AdminPanelValidateDifficulty(diff);
+        }
+     
+        public static string AdminPanelValidationMessageMidiFile(MidiFile file)
+        {
+            return ValidationLogic.AdminPanelValidateMidiFile(file);
+        }
+
 
     }
 }
