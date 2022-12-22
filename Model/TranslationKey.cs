9be71a370f6ca19f6347072d7542fe4c3076a446
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2022-12-22 10:10:40 +0100

    Getting keys

--- Model/TranslationKey.cs
+++ Model/TranslationKey.cs
@@ -5,11 +5,22 @@
     /// </summary>
     public enum TranslationKey
     {
+        Menubar_BackToMain,
+        Menubar_Settings,
+        Menubar_MIDI,
+        Menubar_MIDI_Open,
+        Menubar_MIDI_Play,
+        Menubar_MIDI_Stop,
+        Menubar_MIDI_Isolate,
+
         MainMenu_Settings,
         Settings_Volume,
         Settings_InputDevice,
+        Settings_Language,
 
         MainMenu_Play,
+        Menubar_SongSelect_SelectSong,
+        Menubar_SongSelect_Start,
 
         MainMenu_FreePlay,
     }
