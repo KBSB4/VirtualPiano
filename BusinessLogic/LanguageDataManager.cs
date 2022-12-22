8c7f080341b00a49ba038e74d041bb63836a94db
sandordost <sandor1998@hotmail.com>
2022-12-22 12:05:58 +0100

    LanguageKey and english

--- BusinessLogic/LanguageDataManager.cs
+++ BusinessLogic/LanguageDataManager.cs
@@ -3,74 +3,144 @@ using Newtonsoft.Json;
 
 namespace BusinessLogic
 {
-    public class LanguageDataManager
-    {
-        private Language? currentLanguage;
+	public class LanguageDataManager
+	{
+		private Language? currentLanguage;
 
-        public LanguageDataManager()
-        {
-            CreateLanguages();
-            currentLanguage = GetLanguage(GetPreferredLanguage());
-        }
+		public LanguageDataManager()
+		{
+			CreateLanguages();
+			currentLanguage = GetLanguage(GetPreferredLanguage());
+		}
 
-        private static LanguageData GetLanguageData()
-        {
-            string openedjson = File.ReadAllText("LanguageData.json");
-            return JsonConvert.DeserializeObject<LanguageData>(openedjson);
-        }
+		private static LanguageData GetLanguageData()
+		{
+			string openedjson = File.ReadAllText("LanguageData.json");
+			return JsonConvert.DeserializeObject<LanguageData>(openedjson);
+		}
 
-        public void SetPreferredLanguage(LanguageCode code)
-        {
-            LanguageData languageData = GetLanguageData();
-            languageData.preferredLanguage = code;
-            WriteLanguageData(languageData);
-        }
+		public void SetPreferredLanguage(LanguageCode code)
+		{
+			LanguageData languageData = GetLanguageData();
+			languageData.preferredLanguage = code;
+			WriteLanguageData(languageData);
+		}
 
-        public LanguageCode GetPreferredLanguage()
-        {
-            LanguageData languageData = GetLanguageData();
+		public LanguageCode GetPreferredLanguage()
+		{
+			LanguageData languageData = GetLanguageData();
 
-            return languageData.preferredLanguage;
-        }
+			return languageData.preferredLanguage;
+		}
 
-        private Language? GetLanguage(LanguageCode code)
-        {
-            LanguageData languageData = GetLanguageData();
+		private Language? GetLanguage(LanguageCode code)
+		{
+			LanguageData languageData = GetLanguageData();
 
-            return languageData.languages.Where(lang => lang.Code == code).FirstOrDefault();
-        }
+			return languageData.languages.Where(lang => lang.Code == code).FirstOrDefault();
+		}
 
-        public string GetTranslation(TranslationKey key)
-        {
-            return currentLanguage.Translations[key];
-        }
+		public string GetTranslation(TranslationKey key)
+		{
+			return currentLanguage.Translations[key];
+		}
 
-        //TODO Temp
-        public static void CreateLanguages()
-        {
-            LanguageData languageData = new();
 
-            Language language = new Language();
-            language.Code = LanguageCode.NL;
-            language.Name = "Nederlands";
-            language.Translations = new() {
-                {TranslationKey.MainMenu_Settings, "Instellingen" },
-                {TranslationKey.Settings_Volume, "Volume" },
-                {TranslationKey.Settings_InputDevice, "Ingangsapparaat" },
-                {TranslationKey.MainMenu_Play, "Spelen" },
-                {TranslationKey.MainMenu_FreePlay, "Vrij Spel" }
-            };
+		//Menubar_BackToMain,
+		//      Menubar_Settings,
+		//      Menubar_MIDI_Open,
+		//      Menubar_MIDI_Play,
+		//      Menubar_MIDI_Stop,
+		//      Menubar_MIDI_Karaoke,
 
-            languageData.languages = new();
-            languageData.languages.Add(language);
-            languageData.preferredLanguage = LanguageCode.NL;
-            WriteLanguageData(languageData);
-        }
+		//      MainMenu_Settings,
+		//		MainMenu_FreePlay,
+		//		MainMenu_Play,
 
-        private static void WriteLanguageData(LanguageData languageData)
-        {
-            string rararar = JsonConvert.SerializeObject(languageData, Formatting.Indented);
-            File.WriteAllText("C:\\Users\\Harris\\Source\\Repos\\KBSB4\\VirtualPiano\\BusinessLogic\\LanguageData.json", rararar);
-        }
-    }
+		//		Settings_Volume,
+		//      Settings_InputDevice,
+		//      Settings_Language,
+
+		//      Play_SongFinishedScreen_Title,
+		//      Play_SongFinishedScreen_YourScore,
+		//      Play_SongFinishedScreen_MaxScore,
+		//      Play_SongFinishedScreen_UploadButton,
+		//      Play_SongFinishedScreen_MenuButton,
+
+		//      AccountPage_LoginTitle,
+		//      AccountPage_LoginUsername,
+		//      AccountPage_LoginPassword,
+		//      AccountPage_LoginButton,
+
+		//      AccountPage_RegisterTitle,
+		//      AccountPage_RegisterUsername,
+		//		AccountPage_RegisterEmail,
+		//		AccountPage_RegisterPassword,
+		//		AccountPage_RegisterPasswordConfirm,
+		//		AccountPage_RegisterButton,
+
+		//      MessageBox_HighscoreHigherThan,
+		//      MessageBox_SelectSongBeforeStart,
+		//      MessageBox_LogOut,
+		//      MessageBox_MidiStillPlaying,
+		//      MessageBox_NoMidiSelected,
+		//      MessageBox_NoMidiPlaying,
+
+		//      Menubar_SongSelect_SelectSong,
+		//      Menubar_SongSelect_Start,
+		//      Menubar_SongSelect_Karaoke,
+
+		//TODO Temp
+		public static void CreateLanguages()
+		{
+			LanguageData languageData = new();
+
+			Language dutch = new Language();
+
+			//Nederlands
+			dutch.Code = LanguageCode.NL;
+			dutch.Name = "Nederlands";
+			dutch.Translations = new() {
+                //Main menu
+				{TranslationKey.MainMenu_Settings, "Instellingen" },
+				{TranslationKey.MainMenu_Play, "Spelen" },
+				{TranslationKey.MainMenu_FreePlay, "Vrij Spel" },
+
+                //Settings
+				{TranslationKey.Settings_Volume, "Volume" },
+				{TranslationKey.Settings_InputDevice, "Ingangsapparaat" },
+				{TranslationKey.Settings_Language, "Taal" },
+			};
+
+			//English
+			Language english = new Language();
+			english.Code = LanguageCode.EN;
+			english.Name = "English";
+			english.Translations = new()
+			{
+                //Main menu
+				{TranslationKey.MainMenu_Settings, "Settings" },
+				{TranslationKey.MainMenu_Play, "Play" },
+				{TranslationKey.MainMenu_FreePlay, "Freeplay" },
+
+                //Settings
+				{TranslationKey.Settings_Volume, "Volume" },
+				{TranslationKey.Settings_InputDevice, "Input device" },
+				{TranslationKey.Settings_Language, "Language" },
+
+
+			};
+
+			languageData.languages = new();
+			languageData.languages.Add(dutch);
+			languageData.preferredLanguage = LanguageCode.NL;
+			WriteLanguageData(languageData);
+		}
+
+		private static void WriteLanguageData(LanguageData languageData)
+		{
+			string rararar = JsonConvert.SerializeObject(languageData, Formatting.Indented);
+			File.WriteAllText("C:\\Users\\Harris\\Source\\Repos\\KBSB4\\VirtualPiano\\BusinessLogic\\LanguageData.json", rararar);
+		}
+	}
 }
--- Model/TranslationKey.cs
+++ Model/TranslationKey.cs
@@ -7,22 +7,47 @@
     {
         Menubar_BackToMain,
         Menubar_Settings,
-        Menubar_MIDI,
         Menubar_MIDI_Open,
         Menubar_MIDI_Play,
         Menubar_MIDI_Stop,
-        Menubar_MIDI_Isolate,
+        Menubar_MIDI_Karaoke,
 
         MainMenu_Settings,
-        Settings_Volume,
+		MainMenu_FreePlay,
+		MainMenu_Play,
+
+		Settings_Volume,
         Settings_InputDevice,
         Settings_Language,
 
-        MainMenu_Play,
+        Play_SongFinishedScreen_Title,
+        Play_SongFinishedScreen_YourScore,
+        Play_SongFinishedScreen_MaxScore,
+        Play_SongFinishedScreen_UploadButton,
+        Play_SongFinishedScreen_MenuButton,
+
+        AccountPage_LoginTitle,
+        AccountPage_LoginUsername,
+        AccountPage_LoginPassword,
+        AccountPage_LoginButton,
+
+        AccountPage_RegisterTitle,
+        AccountPage_RegisterUsername,
+		AccountPage_RegisterEmail,
+		AccountPage_RegisterPassword,
+		AccountPage_RegisterPasswordConfirm,
+		AccountPage_RegisterButton,
+
+        MessageBox_HighscoreHigherThan,
+        MessageBox_SelectSongBeforeStart,
+        MessageBox_LogOut,
+        MessageBox_MidiStillPlaying,
+        MessageBox_NoMidiSelected,
+        MessageBox_NoMidiPlaying,
+
         Menubar_SongSelect_SelectSong,
         Menubar_SongSelect_Start,
         Menubar_SongSelect_Karaoke,
 
-        MainMenu_FreePlay,
     }
 }
