1011416d0fb5fd2a2270ac3b79efab6e3ecab9dc
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2023-01-02 12:01:29 +0100

    Moved tests

--- UnitTests_PianoSoundPlayer/BusinessLogic_PianoLogic_Tests.cs
+++ UnitTests_PianoSoundPlayer/BusinessLogic_PianoLogic_Tests.cs
@@ -35,5 +35,36 @@ namespace UnitTests
             Octave pianoKey3 = piano.PianoKeys[0].Octave;
             Assert.That(pianoKey, Is.EqualTo(pianoKey3));
         }
+
+        [TestCase(true, 0, Octave.Four)]
+        [TestCase(false, 0, Octave.Two)]
+        [TestCase(true, 12, Octave.Five)]
+        [TestCase(false, 12, Octave.Three)]
+        public void OctaveGetsIncreased(bool b, int index, Octave result)
+        {
+            if (b)
+            {
+                PianoLogic.SwapOctave(piano);
+            }
+
+            Assert.That(piano.PianoKeys[index].Octave, Is.EqualTo(result));
+        }
+
+        [TestCase(true, 0, Octave.Two)]
+        [TestCase(true, 12, Octave.Three)]
+        public void OctaveGetsDecreased(bool b, int index, Octave result)
+        {
+            if (b)
+            {
+                PianoLogic.SwapOctave(piano);
+            }
+
+            if (b)
+            {
+                PianoLogic.SwapOctave(piano);
+            }
+
+            Assert.That(piano.PianoKeys[index].Octave, Is.EqualTo(result));
+        }
     }
 }
